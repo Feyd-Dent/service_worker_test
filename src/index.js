43f753ac5e3d51870458f262eefe00163e5487d6
Daniel Kennedy <fade.rocks@gmail.com>
2022-01-13 22:39:34 +0000

    first comit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import 'tachyons';
 
 
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
 import { requestRobots, searchRobots } from './reducers'
 
 import './index.css';
@@ -25,4 +25,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
